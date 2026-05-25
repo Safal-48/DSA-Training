5454283859f39abd3dfa0c55044dd5fb860117d1
Safal <palchiku99@gmail.com>
2026-05-25 15:46:14 +0530

    first commit

---
+++ day1/assignment.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+
+    a += 2;
+    cout << a << endl;
+    a-= 2;
+    cout << a << endl;
+    a *= 2;
+    cout << a << endl;                  
+    a /= 2;
+    cout << a << endl;  
+    a %= 2;
+    cout << a << endl;
+    return 0   ;
+}
---
+++ day1/io.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int age=0;
+char name[100]; //variable initaialize
+cout<<"Enter your age:";
+cin>>age;
+cout<<age<<endl;
+cout<<"Enter your name:";
+cin>> name;
+cout<<name;
+ return 0;
+}
+
