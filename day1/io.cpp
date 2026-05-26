26bcc03bd0c883cbd597a561e1b49a8f31760878
Safal <palchiku99@gmail.com>
2026-05-26 11:01:34 +0530

    updated io.cpp

--- day1/io.cpp
+++ day1/io.cpp
@@ -1,16 +1,9 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
-{
-int age=0;
-char name[100]; //variable initaialize
-cout<<"Enter your age:";
-cin>>age;
-cout<<age<<endl;
-cout<<"Enter your name:";
-cin>> name;
-cout<<name;
- return 0;
+int main(){
+    int a;
+    cin>>a;
+    cout<<a;
+    return 0;
 }
-
