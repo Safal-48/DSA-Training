fa505c52c51b6ad020d48231482a97dd673b0f2d
Safal <palchiku99@gmail.com>
2026-06-09 13:13:56 +0530

    added day 10

---
+++ day 10/number-crown-pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+       
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        int spaces = 2 * (n - i);
+        for (int j = 1; j <= spaces; j++)
+        {
+            cout << " ";
+        }
+      
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
