ebcc89991348894b569707f0149ceb0d758fd4b1
Safal <palchiku99@gmail.com>
2026-06-10 15:06:21 +0530

    added day 11

--- day 11/tempCodeRunnerFile.cpp
+++ day 11/tempCodeRunnerFile.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main() {
-    int rows = 5;
-
+    int rows = 5; 
     for (int i = 1; i <= rows; i++) {
-        char ch = 'A';
-
-        for (int j = 1; j <= i; j++) {
-            cout << ch << " ";
-            ch++;
+       
+        for (int j = 'E'-i+1; j <= 'E'; j++) {
+            cout << j << " ";
+            
         }
         cout << endl;
     }
