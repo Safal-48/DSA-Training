bbc23e2d696da37d137aac731e13d012b9a1ab0f
Safal <palchiku99@gmail.com>
2026-07-27 22:45:22 +0530

    added day 25

---
+++ day 25/Move-All-Zeroes-to-the-End-of-an-Array-Using-Two-Pointer-Approach.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveZeroes(vector<int> &nums)
+{
+    int n = nums.size();
+
+    int j = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == 0)
+        {
+            j = i;
+            break;
+        }
+    }
+
+    if (j == -1)
+    {
+        return;
+    }
+
+    for (int i = j + 1; i < n; i++)
+    {
+        if (nums[i] != 0)
+        {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+}
+
+int main()
+{
+   
+    vector<int> v = {1, 0, 1, 3, 0, 12};
+
+    moveZeroes(v);
+
+    for (int x : v)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
