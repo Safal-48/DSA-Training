685245bacbd6d374b422bd47014623f46353559c
Safal <palchiku99@gmail.com>
2026-07-30 20:33:11 +0530

    added day 28

---
+++ day 28/Contains-Duplicate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool containsDuplicate(vector<int> &nums)
+{
+    unordered_set<int> st;
+
+    for (int num : nums)
+    {
+        if (st.find(num) != st.end())
+        {
+            return true;
+        }
+        st.insert(num);
+    }
+
+    return false;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 1};
+
+    cout << containsDuplicate(nums);
+
+    return 0;
+}
