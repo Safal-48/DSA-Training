4adc35079e0420489ae571f88ecdb74318d2966c
Safal <palchiku99@gmail.com>
2026-08-04 07:15:01 +0530

    added day 30

---
+++ day 30/Majority-Element-Using-Brute-Force-Approach.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int majorityElement(vector<int> &nums)
+{
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                cnt++;
+            }
+        }
+
+        if (cnt > n / 2)
+        {
+            return nums[i];
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+  
+    vector<int> v = {2, 2, 1, 1, 1, 2, 2};
+
+    cout << "Majority Element: " << majorityElement(v);
+
+    return 0;
+}
